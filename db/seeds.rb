4f7955fe881a90af2f1c64ccb27ee16d22187518
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-04 16:58:15 -0400

    listo pero feo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+
+random_price = [122, 323, 4312, 1212]
+random_guests = [1, 2, 3, 4, 5, 6, 7]
+5.times do
+  Flat.create(name: Faker::FunnyName.name, address: Faker::Address.street_address,
+              description: Faker::Lorem.paragraph(sentence_count: 2), number_of_guests: random_guests.sample,
+              price_per_night: random_price.sample)
+end
